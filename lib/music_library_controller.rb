1a4849271cabadfe7880d042a4fa1f154124f65c
Sherry El Garhy <sherryptk@gmail.com>
2017-12-01 16:04:28 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -82,7 +82,7 @@ class MusicLibraryController
     response = gets
     number = response.to_i - 1
     sorted = Song.all.sort_by {|obj| obj.name}
-    unless number > sorted.length || number < 1 || number.is_a? Numeric == false
+    unless number > sorted.length || number < 1 || number.is_number?  == false
       puts "Playing #{sorted[number].name} by #{sorted[number].artist.name}"
 
     end
