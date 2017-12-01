b23c66865a509bf1347706bd9b1f44c524fd24c1
Sherry El Garhy <sherryptk@gmail.com>
2017-12-01 15:46:54 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -84,7 +84,7 @@ class MusicLibraryController
     unless number == false
       sorted = Song.all.sort_by {|obj| obj.name}
 
-      puts "Playing #{sorted[number].artist.name} by #{sorted[number].song.name}"
+      puts "Playing #{sorted[number].artist.name} by #{sorted[number].name}"
 
     end
   end
