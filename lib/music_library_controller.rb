9967ba0d4019d59b5ae35fed0aa391ded1ab55f0
Sherry El Garhy <sherryptk@gmail.com>
2017-12-01 17:37:22 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -19,7 +19,9 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
+
       response = gets
+
       if response == 'list songs'
         list_songs
       elsif response == 'list artists'
@@ -32,49 +34,44 @@ class MusicLibraryController
         list_songs_by_genre
       elsif response == 'play song'
         play_song
-
-
-
-
-
       end
     end
   end
 
-    def list_songs
-      count = 1
-      sorted = Song.all.sort_by {|obj| obj.name}
-      sorted.each do |song|
-        puts "#{count}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
-        count+=1
-      end
+  def list_songs
+    count = 1
+    sorted = Song.all.sort_by {|obj| obj.name}
+    sorted.each do |song|
+      puts "#{count}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+      count+=1
     end
+  end
 
-    def list_artists
-      count = 1
-      sorted = Artist.all.sort_by {|obj| obj.name}
-      sorted.each do |artist|
-        puts "#{count}. #{artist.name}"
-        count+=1
-      end
+  def list_artists
+    count = 1
+    sorted = Artist.all.sort_by {|obj| obj.name}
+    sorted.each do |artist|
+      puts "#{count}. #{artist.name}"
+      count+=1
     end
+  end
 
-    def list_genres
-      count = 1
-      sorted = Genre.all.sort_by {|obj| obj.name}
-      sorted.each do |genre|
-        puts "#{count}. #{genre.name}"
-        count+=1
-      end
+  def list_genres
+    count = 1
+    sorted = Genre.all.sort_by {|obj| obj.name}
+    sorted.each do |genre|
+      puts "#{count}. #{genre.name}"
+      count+=1
     end
+  end
 
-    def list_songs_by_artist
-      puts "Please enter the name of an artist:"
-      response = gets
-      artist = Artist.find_by_name(response)
-      count = 1
-      unless artist == nil
-        sorted = artist.songs.sort_by {|obj| obj.name}
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    response = gets
+    artist = Artist.find_by_name(response)
+    count = 1
+    unless artist == nil
+      sorted = artist.songs.sort_by {|obj| obj.name}
         sorted.each do |song|
         puts "#{count}. #{song.name} - #{song.genre.name}"
         count +=1
@@ -89,25 +86,22 @@ class MusicLibraryController
     count = 1
     unless genre == nil
       sorted = genre.songs.sort_by {|obj| obj.name}
-      sorted.each do |song|
-      puts "#{count}. #{song.artist.name} - #{song.name}"
-      count +=1
+        sorted.each do |song|
+        puts "#{count}. #{song.artist.name} - #{song.name}"
+        count +=1
+      end
     end
-  end
 
   def play_song
     puts "Which song number would you like to play?"
     response = gets
     number = response.to_i - 1
     sorted = Song.all.sort_by {|obj| obj.name}
-    if number < sorted.length && number >= 1 && number.is_a?(Integer)
-      puts "Playing #{sorted[number].name} by #{sorted[number].artist.name}"
+      if number < sorted.length && number >= 1 && number.is_a?(Integer)
+        puts "Playing #{sorted[number].name} by #{sorted[number].artist.name}"
+      end
     end
   end
-
-  end
-
-
 end
 
 
